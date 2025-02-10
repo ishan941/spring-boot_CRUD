ee99262807fe52336c120f011d9e11c672133413
Ishan Shrestha <iahan.esan18@gmail.com>
2025-02-10 22:39:38 +0545

    feat: API response for interracting with json

---
+++ src/main/java/com/example/CRUDApplication/apiResponse/ApiResponse.java
@@ -0,0 +1,31 @@
+package com.example.CRUDApplication.apiResponse;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder // design pattern given lumbok
+public class ApiResponse<T> {
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("message")
+    private String messsage;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("StatusCode")
+    private int statusCode;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("Data")
+    private T data;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("ListData")
+    private List<T> listData;
+
+}
