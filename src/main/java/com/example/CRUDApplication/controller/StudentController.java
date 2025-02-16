5abae8195a99c0e67991dbe06af21fe8def0ebf3
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-02-16 14:32:50 +0545

    jwt

--- src/main/java/com/example/CRUDApplication/controller/StudentController.java
+++ src/main/java/com/example/CRUDApplication/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -99,6 +100,7 @@ public class StudentController {
         return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') || hasAuthority('USER')")
     @GetMapping("/getLimited")
     public ResponseEntity<ApiResponse> getLimited() {
         List<ProjectionDto> list = studentService.getPerticularField();
